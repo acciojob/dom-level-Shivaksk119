a50a2cc185f27ed834a0a5a72e277315d277485c
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-25 22:15:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let item = document.getElementId('level');
+
+let count =0;
+let parent = item.parentElement();
+const levelFinder=(parent)=>{
+	while(parent){
+		count++;
+		parent = parent.parentElement;
+	}
+	return count;
+}
+
+alert(levelFinder(parent));
